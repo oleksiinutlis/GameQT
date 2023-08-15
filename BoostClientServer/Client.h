50e320ab93ef3221fffdc7ed1127657ef8933f82
Alex-Tsarenko <ga4yt324@gmail.com>
2023-08-16 01:31:01 +0300

    reworked Client as async (7)

--- BoostClientServer/Client.h
+++ BoostClientServer/Client.h
@@ -74,7 +74,7 @@ public:
         // Receive the response from the server
         char data[1024];
         size_t len = socket.read_some(buffer(data));
-        std::cout << "Received: " << data << std::endl;
+        std::cout << "Received: " << std::string( data, len ) << std::endl;
         readResponse();
         return;
 
