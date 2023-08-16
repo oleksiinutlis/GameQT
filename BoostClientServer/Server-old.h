f8ab9b5e06daaa13bf5069e5a35da36c320a9659
Alex-Tsarenko <ga4yt324@gmail.com>
2023-08-16 22:40:21 +0300

    added Server-old.h

---
+++ BoostClientServer/Server-old.h
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <map>
+#include <boost/asio.hpp>
+
+using namespace boost::asio;
+using ip::tcp;
+
+class IClientSession
+{
+public:
+    virtual void sendMessage( std::string message ) = 0;
+    virtual ~IClientSession() = default;
+};
+
+class ClientSession: public IClientSession
+{
+    tcp::socket             m_socket;
+    boost::asio::streambuf  m_streambuf;
+
+public:
+    ClientSession( io_context& ioContext ) : m_socket( ioContext )
+    {
+    }
+    
+    ~ClientSession()
+    {
+        std::cout << "!!!! ~ClientSession()" << std::endl;
+    }
+    
+    tcp::socket& socket() { return m_socket; }
+    
+    virtual void sendMessage( std::string command ) override
+    {
+        async_write( m_socket, buffer( command+"\n" ),
+            [] ( const boost::system::error_code& ec, std::size_t bytes_transferred  )
+            {
+                if ( ec )
+                {
+                    std::cout << "!!!! ClientSession::sendMessage error: " << ec.message() << std::endl;
+                    exit(-1);
+                }
+            });
+    }
+
+    void readMessage()
+    {
+        async_read_until( m_socket, m_streambuf, '\n',
+            [this] ( const boost::system::error_code& ec, std::size_t bytes_transferred )
+            {
+                if ( ec )
+                {
+                    std::cout << "!!!! ClientSession::readMessage error: " << ec.message() << std::endl;
+                    exit(-1);
+                }
+                else
+                {
+                    std::cout << "Received: " << std::string( (const char*)m_streambuf.data().data(), m_streambuf.size() ) << std::endl;
+                    sendMessage( "WaitingSecondPlayer;" );
+                }
+        });
+    }
+};
+
+class Server
+{
+    io_context      ioContext;
+    tcp::acceptor   acceptor;
+    
+    std::vector<ClientSession*> m_sessions;
+
+public:
+    Server( int port ) :
+        acceptor( ioContext, tcp::endpoint(tcp::v4(), port) )
+    {
+    }
+
+    void execute()
+    {
+        post( ioContext, [this] { accept(); } );
+        ioContext.run();
+    }
+    
+    void accept()
+    {
+        auto* clientSession = new ClientSession( ioContext );
+        m_sessions.push_back( clientSession );
+        
+        acceptor.async_accept( clientSession->socket(), [ this, clientSession ] ( boost::system::error_code ec )
+        {
+            if ( ec )
+            {
+                std::cout << "!!!! async_accept error: " << ec.message() << std::endl;
+                exit(-1);
+            }
+            //std::cout << "Client connected: " << clientSession->socket().remote_endpoint() << std::endl;
+            clientSession->readMessage();
+
+            accept();
+        });
+    }
+};
--- BoostClientServer/test.cpp
+++ BoostClientServer/test.cpp
@@ -23,34 +23,34 @@ int main()
 //        client.execute( "127.0.0.1", 12345, "StartGame;001;800;600;" );
 //    }).detach();
 
-//    Client client2;
-//    client2.execute( "127.0.0.1", 12345, "StartGame;001;1000;800;" );
+    Client client2;
+    client2.execute( "127.0.0.1", 12345, "StartGame;001;1000;800;" );
 
-    try {
-        io_context ioContext;
-        tcp::socket socket(ioContext);
-
-        // Connect to the server
-        socket.connect(tcp::endpoint(ip::address::from_string("127.0.0.1"), 12345));
-
-        // Send a message to the server
-        std::string message = "Hello from client!;\n";
-        write(socket, buffer(message));
-
-        // Receive the response from the server
-        char data[1024];
-        size_t len = socket.read_some(buffer(data));
-        std::cout << "Received from server: " << data << std::endl;
-
-        len = socket.read_some(buffer(data));
-        std::cout << "(2) Received from server: " << data << std::endl;
-
-        // Close the connection
-        socket.close();
-    }
-    catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
+//    try {
+//        io_context ioContext;
+//        tcp::socket socket(ioContext);
+//
+//        // Connect to the server
+//        socket.connect(tcp::endpoint(ip::address::from_string("127.0.0.1"), 12345));
+//
+//        // Send a message to the server
+//        std::string message = "Hello from client!;\n";
+//        write(socket, buffer(message));
+//
+//        // Receive the response from the server
+//        char data[1024];
+//        size_t len = socket.read_some(buffer(data));
+//        std::cout << "Received from server: " << data << std::endl;
+//
+//        len = socket.read_some(buffer(data));
+//        std::cout << "(2) Received from server: " << data << std::endl;
+//
+//        // Close the connection
+//        socket.close();
+//    }
+//    catch (std::exception& e) {
+//        std::cerr << "Exception: " << e.what() << std::endl;
+//    }
 
     return 0;
 }
