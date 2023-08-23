3ffcebca151dbf4dd5a4e6496ab74f211b5a69dd
Alex-Tsarenko <ga4yt324@gmail.com>
2023-08-23 23:12:21 +0300

    commit error

--- BoostClientServer/test.cpp
+++ BoostClientServer/test.cpp
@@ -10,20 +10,20 @@ int main()
 {
     std::thread( []
     {
-        TcpServer server(12345);
+        TcpServer server(1234);
         server.execute();
     }).detach();
 
-    //std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
     
     std::thread( []
     {
         Client client;
-        client.execute( "127.0.0.1", 12345, "StartGame;001;800;600;" );
+        client.execute( "127.0.0.1", 1234, "StartGame;001;800;600;" );
     }).detach();
 
     Client client2;
-    client2.execute( "127.0.0.1", 12345, "StartGame;001;1000;800;" );
+    client2.execute( "127.0.0.1", 1234, "StartGame;001;1000;800;" );
 
     return 0;
 }
